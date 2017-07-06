cadd05de6e8f03f08f7ddd0f101690d90cc5b04c
JIMMY NGUYEN <JIMMY.NGUYEN@adtran.com>
2017-07-06 13:47:04 -0500

    adding seahorse

---
+++ Seahorse/Seahorse.py
@@ -0,0 +1,28 @@
+from botlib import Bot, Group, Trigger
+import json
+
+server = "coop.test.adtran.com"
+channel = "#main"
+nick = "Seahorse"
+def start_bot(item):
+    bot = Bot(item['server'], item['channel'], item['nick'], item['ident'])
+    for trigger in item['triggers']:
+        groups = []
+        regex = trigger['pattern']
+        if 'isCommand' not in trigger:
+            trigger['isCommand'] = False
+        for key, value in trigger.items():
+            if key in ('responses', 'pattern', 'isAction', 'isCommand'):
+                continue
+            groups.append(Group(key, *value))
+        bot.add_trigger(Trigger(regex, trigger['responses'], trigger['isAction'], trigger['isCommand'], *groups))
+    return bot
+
+
+def main():
+    with open(r'Seahorse.json') as f:
+        config = json.load(f)
+    bot = start_bot(config)
+    bot.act()
+
+main()
