1afc147a23e418977b8773e394724c5446d16c39
Noah Siano <nsiano@bellsouth.net>
2018-01-16 12:45:46 -0600

    theCount will not start a new game if a game is still in session

--- theCount/countBot.py
+++ theCount/countBot.py
@@ -148,14 +148,15 @@ class countBot(irc.IRCClient):
         return
 
     def automateStart(self):
-        hour = int(self.getCurrentTime().split(':')[0])
-        minute = int(self.getCurrentTime().split(':')[1])
-        if ((hour != self.hourOfLastGame) and (hour > 7 and hour < 17)):
-            if (((hour == 8) and (minute >= 30)) or ((hour == 11) and (minute >= 0)) or
-            ((hour == 13) and (minute >= 30)) or ((hour == 16) and (minute >= 0))):
-                self.hourOfLastGame = hour
-                self.resetGame()
-                self.startGame()
+        if (!self.gameRunning):
+            hour = int(self.getCurrentTime().split(':')[0])
+            minute = int(self.getCurrentTime().split(':')[1])
+            if ((hour != self.hourOfLastGame) and (hour > 7 and hour < 17)):
+                if (((hour == 8) and (minute >= 30)) or ((hour == 11) and (minute >= 0)) or
+                ((hour == 13) and (minute >= 30)) or ((hour == 16) and (minute >= 0))):
+                    self.hourOfLastGame = hour
+                    self.resetGame()
+                    self.startGame()
 
     def getWinningUser(self):
         topUser = player("")
