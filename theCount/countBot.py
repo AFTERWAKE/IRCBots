99e791a073810695c015f5c9b2962cceb5306fc8
Noah Siano <nsiano@bellsouth.net>
2018-02-13 09:43:28 -0600

    Update rules in .py file to reflect actual rules

--- theCount/countBot.py
+++ theCount/countBot.py
@@ -11,15 +11,21 @@
                    botNick: del <userNick> (delete a user from the list/winnings table)
                    botNick: start (starts game)
                    botNick: save (saves list of winners || also gets saved at the end of every game)
-                   botNick: quit (quits current game)
+                   botNick, stop (quits current game)
                    botNick: users (prints list of users to console)
                    botNick: restore (restores winners from save file || also restores automatically on run)
                    botNick: say <msg> (sends message to channel as the bot)
+                   botNick, mute <user> (mutes a user by IP, they will be ignored for commands and will not be able to play the game)
+                   botNick, unmute <user> (undoes the actions of the `mute` command)
+                   botNick, whois <user> (Gives the IP address of a user on the server)
+                   botNick quit <msg>{optional} (the bot leaves the channel, with an optional quit message)
               USER COMMANDS
                    botNick: help (help message)
                    botNick: loser (LOSER: <user who called>)
                    botNick: losers (list of losers)
                    botNick: winners (shows list of winners)
+                   botNick, rules (shows list of rules)
+                   botNick, version (shows version + link to github)
 --------------------------------------------------------------------------------------------------------------------
 '''
 from twisted.words.protocols import irc
