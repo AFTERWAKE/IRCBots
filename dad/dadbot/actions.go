13b5da39a82b572d7ee2446b222f59258268b59e
alecwest <alec.west@adtran.com>
2018-03-02 13:29:17 -0600

    Stupid new alias commands don't add like I thought they did

---
+++ dad/dadbot/actions.go
@@ -0,0 +1,72 @@
+// Package dad is an extension of hellabot that plays the role of an IRC
+// chat bot, either as a mom or a dad
+package dad
+
+import (
+  "regexp"
+  "strings"
+)
+
+// PerformAction evaluates the action associated with the response (speak)
+// and performs any necessary actions. Any content that needs to be used in
+// the response will be returned.
+func PerformAction(reply Reply, speak SpeakData,
+    variable string) (Reply, string) {
+    // Handle any included action
+    ground := regexp.MustCompile("(?i)^ground$")
+    unground := regexp.MustCompile("(?i)^unground$")
+    grounded := regexp.MustCompile("(?i)^grounded$")
+    message := regexp.MustCompile("(?i)^message$")
+    if ground.MatchString(speak.Action) {
+        Ground(variable)
+    }
+    if unground.MatchString(speak.Action) {
+        Unground(variable)
+    }
+    if grounded.MatchString(speak.Action) {
+        variable = strings.Join(Dbot.Grounded, ", ")
+    }
+    if message.MatchString(speak.Action) {
+        to, msg := FormatMessage(variable, speak)
+        if len(to) > 0 {
+            reply.To = to
+        }
+        variable = msg
+    }
+    return reply, variable
+}
+
+// Ground checks the list of currently grounded users and adds the name if
+// it has not yet been added.
+func Ground(name string) {
+    i := StringInSlice(name, Dbot.Grounded)
+    if i != -1 {
+        return
+    }
+    Dbot.Grounded = append(Dbot.Grounded, name)
+}
+
+// Unground checks the list of grounded users for the requested name and
+// removes it if it is found.
+func Unground(name string) {
+    i := StringInSlice(name, Dbot.Grounded)
+    if i == -1 {
+        return
+    }
+    Dbot.Grounded = append(Dbot.Grounded[:i], Dbot.Grounded[i+1:]...)
+}
+
+// FormatMessage splits message into its destination and message components
+// (formatted as <recipient>: <rest>). It returns the recipient,
+// or the bot's primary channel if a recipient was not specified,
+// and the actual message
+func FormatMessage(message string, s SpeakData) (string, string) {
+    to := ""
+    message = RemoveTriggerRegex(message, s.Regex)
+    to = RemoveLiteralRegex(message, ":.*")
+    if to == message {
+        to = Dbot.Channels[0]
+    }
+    message = RemoveLiteralRegex(message, ".*:\\s")
+    return to, message
+}
---
+++ dad/dadbot/config.go
@@ -0,0 +1,67 @@
+// Package dad is an extension of hellabot that plays the role of an IRC
+// chat bot, either as a mom or a dad
+package dad
+
+import (
+  "encoding/json"
+  "io/ioutil"
+  "os"
+)
+
+// ReadConfig reads each config file and returns a struct of each
+func ReadConfig() (IRCConfig, DadConfig, MomConfig) {
+	return readIrcConfig(), readDadConfig(), readMomConfig()
+}
+
+func readIrcConfig() IRCConfig {
+    file, _ := os.Open(ircConfigFile)
+    defer file.Close()
+    decoder := json.NewDecoder(file)
+    conf := IRCConfig{}
+    err := decoder.Decode(&conf)
+    checkErr(err)
+    return conf
+}
+
+func readDadConfig() DadConfig {
+    file, _ := os.Open(dadConfigFile)
+    defer file.Close()
+    decoder := json.NewDecoder(file)
+    conf := DadConfig{}
+    err := decoder.Decode(&conf)
+    checkErr(err)
+    return conf
+}
+
+func readMomConfig() MomConfig {
+    file, _ := os.Open(momConfigFile)
+    defer file.Close()
+    decoder := json.NewDecoder(file)
+    conf := MomConfig{}
+    err := decoder.Decode(&conf)
+    checkErr(err)
+    return conf
+}
+
+// UpdateConfig dispatches a call to the appropriate update function
+// based on which bot is being run (mom or dad), parsing the current
+// config information and rewriting it to the appropriate config file.
+func UpdateConfig() {
+	if Dad {
+		updateDadConfig()
+	} else {
+		updateMomConfig()
+	}
+}
+
+func updateDadConfig() {
+    jsonData, err := json.MarshalIndent(Dbot, "", "    ")
+    checkErr(err)
+    ioutil.WriteFile(dadConfigFile, jsonData, 0644)
+}
+
+func updateMomConfig() {
+    jsonData, err := json.MarshalIndent(Mbot, "", "    ")
+    checkErr(err)
+    ioutil.WriteFile(momConfigFile, jsonData, 0644)
+}
