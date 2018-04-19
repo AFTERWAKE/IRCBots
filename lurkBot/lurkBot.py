4312c96cafeec8d84458705d566eaac6c5183a1b
Noah Siano <nsiano@bellsouth.net>
2018-04-19 14:22:05 -0500

    add cadams

--- lurkBot/lurkBot.py
+++ lurkBot/lurkBot.py
@@ -16,6 +16,7 @@ class LurkBot(irc.IRCClient):
                     "benji",
                     "berNs",
                     "cramey",
+                    "cadams",
                     "emturn",
                     "Isaiah",
                     "jlong",
