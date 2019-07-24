602d98385040de99a84344294e4b16e4f28c10bf
Grace Beal <gywbeal@gmail.com>
2019-07-23 19:37:57 -0500

    refactor(tom): clean up regex to use /b + add help message! (#31)
    * tom: refactor fight song
    
    * correct channel name to #main
    
    * clean up regex
    
    * tom: add help message
    
    * add rolt vs r-olt
    
    * sorry jsheffie
    
    * clean up the code
    
    * Update tom.py

--- tom/tom.py
+++ tom/tom.py
@@ -1,11 +1,8 @@
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol, defer
 from re import search, IGNORECASE
-from random import (
-                    choice,
-                    randint
-                    )
-import random
+from random import choice
+
 import time
 import os
 
@@ -29,8 +26,7 @@ class complimentBot(irc.IRCClient):
     owner_name = ""
     currentTime = 0
     default = 'burn berNs'
-    botList = [
-        "dad", "Seahorse", "pointbot", "botProtector",
+    botList = ["dad", "Seahorse", "pointbot", "botProtector",
         "QuipBot", "MemeBot", "theCount", "Doge", "Calculator", "complimentBot"]
     user_list = []
     ignoreList = []
@@ -67,20 +63,15 @@ class complimentBot(irc.IRCClient):
         usr["nick"] = nargs[1][5]
         usr["host"] = nargs[1][2]
         usr["ip"] = nargs[1][3]
-        # for (key, value) in usr:
-        #     usr[key] = [(value)]
         if (usr["ip"] == self.owner):
                self.owner_name = usr["nick"]
         self.user_list.append(usr)
-        # print self.user_list    
+	
     def irc_RPL_ENDOFWHO(self, *nargs):
             "Called when WHO output is complete"
             print ("Users:")
             for each in self.user_list:
                 print (each["nick"] + each["ip"])
-				#print (each["ip"])
-	# def join(self, channel)
- #    	self.join(channel)	
 
     def privmsg(self, user, channel, message):
 		
@@ -90,40 +81,44 @@ class complimentBot(irc.IRCClient):
         user_name = []
         for name in self.user_list: user_name.append(name["nick"])
 
-        if search(r'(^|\s)+idea *(\s|$)+', message, IGNORECASE):
+        if search(self.nickname+ r",*\shelp", message, IGNORECASE):
+            self.currentTime = timeRightNow
+            self.msg(self.channel, "ADTRAN, Inc. is a provider of telecommunications networking equipment and "
+                                   "internetworking products. Its headquarters are in Huntsville, Alabama.")
+        elif search(r"(\bidea\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "That's a great idea!!")
-        elif search(r'(^|\s)+hackathon *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bhackathon\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "Woooo! Hackathon!!!!")
-        elif search(r'(^|\s)+adtran *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\badtran\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "Goooooo ADTRAN!") 
-        elif search(r'(^|\s)+tom *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\btom\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "It's me! Tom!!! On the eighth floor!!!!") 
-        elif search(r'(^|\s)+TA5K *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bta5k\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "You should go with the flamethrower attachment") 
-        elif search(r'(^|\s)+TA5Ks *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bta5ks\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "our pride and joy the TA5K")
-        elif search(r'(^|\s)+r-olt *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\b(rolt|r-olt)\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "Tomothy Remote-Optical-Line-Terminal Stanton is the name, don't wear it out ;)")
-        elif search(r'(^|\s)+dynetics *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bdynetics\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "no cursing in adtran go wash your mouth")
-        elif search(r'(^|\s)+work *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bwork\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "werk werk werk werk")
-        elif search(r'(^|\s)+Calix *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bcalix\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "no sir")
-        elif search(r'(^|\s)+working *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bworking\b)", message, IGNORECASE):
             self.currentTime = timeRightNow
             self.msg(self.channel, "keep working hard!!!!")
-        elif search(r'(^|\s)+song *(\s|$)+', message, IGNORECASE):
+        elif search(r"(\bfight\ssong\b)", message, IGNORECASE):
             self.msg(self.channel, "GO ADTRAN!!! DUN DUN DUNNNN!!!")
             self.currentTime = timeRightNow
             self.msg(self.channel, random.choice(fightSongLyrics))
@@ -137,7 +132,6 @@ class complimentBot(irc.IRCClient):
             self.msg(self.channel, "*applause*")
 
 
-
 def main():
     f = protocol.ReconnectingClientFactory()
     f.protocol = complimentBot
@@ -145,5 +139,6 @@ def main():
     reactor.connectTCP(serv_ip, serv_port, f)
     reactor.run()
 
+	
 while 1:
     main()
