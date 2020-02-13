208d686dedfc4cc34bb95d9b99641a0a86cbc061
jlogerqu <jason.logerquist@adtran.com>
2020-02-13 09:08:11 -0600

    Add doge example

---
+++ doge/README.md
@@ -0,0 +1,34 @@
+Doge Bot!
+=============
+
+Very bot. Such wow.
+
+Setup
+-----
+
+Prerequisites:
+
+* Twisted (pip install Twisted)
+
+1) `git clone https://github.com/AFTERWAKE/IRCBots.git`
+
+2) `cd IRCBots/doge;`
+
+3) `pip install Twisted`
+
+4) Edit doge.py to configure the bot (see config section below)
+
+5) run `python doge.py` to start the bot
+
+Config
+------
+
+chatroom: The channel you want your bot in
+
+serv_ip: The IP or web address of your IRC server
+
+
+Twisted Documentation
+---------------
+
+http://twistedmatrix.com/documents/current/api/twisted.words.protocols.irc.IRC.html
---
+++ doge/doge.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
+from re import search, IGNORECASE
+from random import randint
+import time
+
+serv_ip = "10.4.163.34"
+serv_port = 6667
+
+class Doge(irc.IRCClient):
+    nickname = "Doge"
+    chatroom = "#main"
+    timeLastCommand = 0
+    timeLastTreatCommand = 0
+    owner = ["172.22.117.48", "172.22.116.80", "nsiano800w10.adtran.com", "jlogequ@jlogerqubox.adtran.com"]
+    willDoTrick = False
+
+    def signedOn(self):
+        self.join(self.chatroom)
+
+    def privmsg(self, user, channel, message):
+        isOwner = user.split('@')[1] in self.owner
+        if (message.startswith(self.nickname)):
+            # if isOwner or (randint(0, 3) == 1):
+                timeRightNow = time.time()
+                if (((timeRightNow - self.timeLastCommand) > 2) or isOwner):
+                    if message == (self.nickname + ', help'):
+                        self.describe(self.chatroom, "bark bark bark, bark, bark woof, bork bark, bark bork h*ck bark bark woof")
+                    if (self.willDoTrick or isOwner):
+                        self.timeLastCommand = time.time()
+                        if message == (self.nickname + ', sit'):
+                            if randint(0,6) == 1:
+                                self.needsMoreTreats()
+                            self.describe(self.chatroom, "sits")
+                        elif message == (self.nickname + ', roll over'):
+                            if randint(0,6) == 1:
+                                self.needsMoreTreats()
+                            self.describe(self.chatroom, "rolls over")
+                        elif message == (self.nickname + ', shake'):
+                            if randint(0,6) == 1:
+                                self.needsMoreTreats()
+                            self.describe(self.chatroom, "lifts up paw")
+                        elif message == (self.nickname + ', play dead'):
+                            if randint(0,6) == 1:
+                                self.needsMoreTreats()
+                            self.describe(self.chatroom, "lays down dramatically")
+                        elif message == (self.nickname + ', speak'):
+                            if randint(0,6) == 1:
+                                self.needsMoreTreats()
+                            if randint(0, 5) == 1:
+                                self.describe(self.chatroom, "borks")
+                            else:
+                                self.describe(self.chatroom, "barks")
+        if search(r"(^|\s)+treats*(!|\?)*(\s|$)+", message, IGNORECASE):
+            timeRightNow = time.time()
+            if ((timeRightNow - self.timeLastTreatCommand) > 2) or isOwner:
+                if (self.willDoTrick == False):
+                    self.timeLastTreatCommand = time.time()
+                    self.describe(self.chatroom, "perks his head up")
+                    self.willDoTrick = True
+        elif search(r"(^|\s)+good boy!*(\s|$)+", message, IGNORECASE):
+            timeRightNow = time.time()
+            if ((timeRightNow - self.timeLastCommand) > 2) or isOwner:
+                self.timeLastCommand = time.time()
+                self.describe(self.chatroom, "barks")
+        elif (search(r"(^|\s)+wow!*(\s|$)+", message, IGNORECASE) or
+        search(r"(^|\s)+very(\s|$)+", message, IGNORECASE) or
+        search(r"(^|\s)+such(\s|$)+", message, IGNORECASE)):
+            timeRightNow = time.time()
+            if ((timeRightNow - self.timeLastCommand) > 4) or isOwner:
+                self.timeLastCommand = time.time()
+                self.msg(self.chatroom, "Wow!")
+
+    def needsMoreTreats(self):
+        self.willDoTrick = False
+        self.describe(self.chatroom, "wanders away")
+
+
+def main():
+    f = protocol.ReconnectingClientFactory()
+    f.protocol = Doge
+
+    reactor.connectTCP(serv_ip, serv_port, f)
+    reactor.run()
+
+
+if __name__ == "__main__":
+    main()
